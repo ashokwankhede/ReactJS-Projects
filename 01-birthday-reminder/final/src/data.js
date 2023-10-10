42183fc36430472b2958eec73e9723ba7c757dc7
Ashok Wankhede <wankhedeashok9822@gmail.com>
2023-10-10 23:13:03 +0530

    Birthday Remainder

---
+++ 01-birthday-reminder/final/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Ashok Wankhede',
+    age: 29,
+    image:
+      'https://igimages.gumlet.io/telugu/home/prabhas24072021_1c.jpg?w=376&dpr=2.6',
+  },
+  {
+    id: 2,
+    name: 'Ajit Nawade',  
+    age: 32,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 3,
+    name: 'Abhishek Dayal',
+    age: 36,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Monu Khurde',
+    age: 34,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+  {
+    id: 5,
+    name: 'Ajay Sinha',
+    age: 29,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY9-6YS6XiVOTHskbsJ8fWdz88hnJrNOxeTRH4MlkfAqn7NNrOemseH8sJyyIRnVN_9AY&usqp=CAU',
+  },
+];
